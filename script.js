cdefbcaeffc8d65d4d1397f4c51af01e4234ba78
Hon Lam <lamh85@gmail.com>
2015-11-23 19:51:02 -0800

    Instantiated the full deck.

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-var displayNumbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+var displayNumbers = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
 var displaySuits = ["diamonds", "clubs", "hearts", "spades"];
 
+var deck = [];
+
 var Card = function(numberStrength, suitStrength) {
-    this.suitStrength = suiteStrength;
+    this.suitStrength = suitStrength;
     this.numberStrength = numberStrength;
     this.display = {
-        this.suit = displaySuits[suiteStrength - 1];
-        this.number = displayNumbers[numberStrength - 1];
+        number: displayNumbers[numberStrength - 1],
+        suit: displaySuits[suitStrength - 1],
+        full: displayNumbers[numberStrength - 1] +" of "+ displaySuits[suitStrength - 1]
+    }
+}
+
+for (numIndex = 1; numIndex <= 13; numIndex++) {
+    for (suitIndex = 1; suitIndex <= 4; suitIndex++) {
+        deck.push(
+            new Card(numIndex, suitIndex)
+        )
     }
 }
